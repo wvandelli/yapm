2a9ee8d3ef4d68f815ad4464d0af7efba2394aa6
Argyris Zardilis <argyris.zardilis@gmail.com>
2013-08-07 10:46:41 +0200

    added a list of sfos to the farm dictionary passed to the partition making script

--- farm_gen.py
+++ farm_gen.py
@@ -20,12 +20,16 @@ dcm_segments = []
 for idx, rack in enumerate(dcm_racks):
     worker_nodes = rack[0:-1]
     service_node = rack[-1]
-    dcm_segments.append(dcm_segment("HLT-Segment-%02d" % idx, service_node, worker_nodes, service_node, service_node))
+    dcm_segments.append(dcm_segment("HLT-Segment-%02d" % idx, service_node,
+                                    worker_nodes, service_node, service_node))
 
 hltsv_host = testbed_hosts['pc-tbed-r3-01']
 controller_host = testbed_hosts['pc-tbed-r3-02']
 
+sfos = pm.farm.subselect_pattern(testbed_hosts,
+                                 'pc-tbed-r3-[%02d-%02d]' % (21,22))
 partition_name = "azar_test"
-farm_dict = partition(partition_name, controller_host, dcm_segments, hltsv_host)
+farm_dict = partition(partition_name, controller_host, dcm_segments,
+                      hltsv_host, sfos)
 pprint(farm_dict)
 
