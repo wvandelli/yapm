1b5130d1d2df1705a682e3dc2ec70d10d2963302
Argyris Zardilis <argyris.zardilis@gmail.com>
2013-08-13 10:54:43 +0200

    stylistic changes

--- pm_hltsv.py
+++ pm_hltsv.py
@@ -7,32 +7,32 @@ from pm.dal import dal, DFdal
 from config.dal import module as dal_module
 from pm_common import create_aggregator_app
 
-def create_top_gatherer(db):
+def create_top_gatherer(config_db):
     gatherer_dal = dal_module("gatherer_dal",
                               'daq/schema/MonInfoGatherer.schema.xml')
-    gatherer_algorithm = db.getObject("MIGAlgorithm",
+    gatherer_algorithm = config_db.getObject("MIGAlgorithm",
                                       "DefaultGathererAlgorithm")
-    info_handler = db.getObject("MIGInformationHandler",
+    info_handler = config_db.getObject("MIGInformationHandler",
                                 "DefaultGathererInformationHandler")
     gatherer_config_top = (gatherer_dal.
                            MIGConfiguration("GathererConfiguration-Top"))
     gatherer_config_top.ProviderRegExp = "Histogramming-.*"
-    top_histo_server = db.getObject("InfrastructureApplication",
+    top_histo_server = config_db.getObject("InfrastructureApplication",
                                     "Histogramming")
     gatherer_config_top.SourceServers = [top_histo_server]
     gatherer_config_top.DestinationServers = [top_histo_server]
     gatherer_config_top.InformationHandler = info_handler
     gatherer_config_top.Algorithm = gatherer_algorithm
-    db.updateObjects([gatherer_config_top])
+    config_db.updateObjects([gatherer_config_top])
 
     top_gatherer_app = gatherer_dal.MIGApplication("Gatherer-Top")
-    gatherer_bin = db.getObject("Binary", "MonInfoGatherer")
+    gatherer_bin = config_db.getObject("Binary", "MonInfoGatherer")
     top_gatherer_app.Program = gatherer_bin
     top_gatherer_app.Configurations = [gatherer_config_top]
-    db.updateObjects([top_gatherer_app])
+    config_db.updateObjects([top_gatherer_app])
     return top_gatherer_app
 
-def create_sfo_application(db, number, host):
+def create_sfo_application(config_db, number, host):
     """
     Creates an SFO application with a specific id(number argument) to be used
     in its name. So for example an SFO taking as argument number 1 will be named
@@ -47,9 +47,10 @@ def create_sfo_application(db, number, host):
     sfo_config.DirectoryWritingTime = 60
     sfo_config.DirectoryChangeTime = 15
     sfo_config.DirectoryToWriteIndex = "/tmp"
-    efio_config = db.getObject("EFIOConfiguration", "EFIO-Configuration-1")
+    efio_config = config_db.getObject("EFIOConfiguration",
+                                      "EFIO-Configuration-1")
     sfo_config.EFIOConfiguration = efio_config
-    db.updateObjects([sfo_config])
+    config_db.updateObjects([sfo_config])
 
     #now create the application itself
     sfo_app = DFdal.SFOApplication("SFO-"+number)
@@ -58,62 +59,63 @@ def create_sfo_application(db, number, host):
     sfo_app.RestartableDuringRun = True
     sfo_app.IfDies = "Restart"
     sfo_app.RunsOn = host
-    sfo_binary = db.getObject("Binary", "SFOng_main")
+    sfo_binary = config_db.getObject("Binary", "SFOng_main")
     sfo_app.Program = sfo_binary
     sfo_app.SFOConfiguration = sfo_config
 
     dcm_dal = dal_module("is_dal", 'daq/schema/dcm.schema.xml')
     dc_is_resource = dcm_dal.DC_ISResourceUpdate("DCAppConf-" + number +
-                                "-ISResourceUpdate-SFO")
+                                                 "-ISResourceUpdate-SFO")
     dc_is_resource.name = "SFO"
     dc_is_resource.delay = 10
     dc_is_resource.activeOnNodes.append("SFO")
-    db.updateObjects([dc_is_resource])
+    config_db.updateObjects([dc_is_resource])
 
     dc_app_config = dcm_dal.DCApplicationConfig("DCAppConfig-"+number)
     dc_app_config.ISDefaultRsrcUpdateInterval = 5
     dc_app_config.ISDefaultServer = "*DF"
     dc_app_config.refDC_ISResourceUpdate.append(dc_is_resource)
-    db.updateObjects([dc_app_config])
+    config_db.updateObjects([dc_app_config])
 
     sfo_app.DFApplicationConfig = dc_app_config
     
-    db.updateObjects([sfo_app])
+    config_db.updateObjects([sfo_app])
     return sfo_app
 
-def create_hltsv_app(db, hltsv_host):
+def create_hltsv_app(config_db, hltsv_host):
     hltsv_dal = dal_module("hltsv_dal", 'daq/schema/hltsv.schema.xml')
-    is_dal = dal_module("is_dal", 'daq/schema/dcm.schema.xml')
     
-    def_config_rules = db.getObject("ConfigurationRuleBundle",
+    def_config_rules = config_db.getObject("ConfigurationRuleBundle",
                          "DefaultConfigurationRuleBundle")
     
     roib_plugin = hltsv_dal.RoIBPluginInternal("plugin_internal")
     roib_plugin.Libraries.append("libsvl1internal")
-    db.updateObjects([roib_plugin])
+    config_db.updateObjects([roib_plugin])
 
-    hltsv_main = db.getObject("Binary", "hltsv_main")
+    hltsv_main = config_db.getObject("Binary", "hltsv_main")
     
     hltsv_app = hltsv_dal.HLTSVApplication("HLTSV")
     hltsv_app.ConfigurationRules = def_config_rules
     hltsv_app.RoIBInput = roib_plugin
     hltsv_app.Program = hltsv_main
     hltsv_app.RunsOn = hltsv_host
-    db.updateObjects([hltsv_app])
+    config_db.updateObjects([hltsv_app])
     
     return hltsv_app
 
-def create_hlt_segment(db, default_host, hltsv_host, sfos):
-    top_gatherer_app = create_top_gatherer(db)
+def create_hlt_segment(config_db, default_host, hltsv_host, sfos):
+    top_gatherer_app = create_top_gatherer(config_db)
     
     hltsv_segment = dal.Segment("HLT")
-    defrc_controller = db.getObject("RunControlTemplateApplication", "DefRC")
-    hltsv_segment.IsControlledBy = defrc_controller
-    hltsv_app = create_hltsv_app(db, hltsv_host)
+    defrc_controller = config_db.getObject("RunControlTemplateApplication",
+                                           "DefRC")
+    hltsv_segment.IsControlleconfig_dby = defrc_controller
+    hltsv_app = create_hltsv_app(config_db, hltsv_host)
 
     sfo_apps = []
     for index, sfo_host in enumerate(sfos):
-        sfo_application = create_sfo_application(db, str(index), sfo_host)
+        sfo_application = create_sfo_application(config_db, str(index),
+                                                 sfo_host)
         sfo_apps.append(sfo_application)
         
     hltsv_resources = [hltsv_app, top_gatherer_app] + sfo_apps
@@ -121,18 +123,18 @@ def create_hlt_segment(db, default_host, hltsv_host, sfos):
     
     hltsv_segment.DefaultHost = default_host
 
-    aggregator_app = create_aggregator_app(db, "top_aggregator.py",
+    aggregator_app = create_aggregator_app(config_db, "top_aggregator.py",
                              default_host)
     hltsv_segment.Applications.append(aggregator_app)
 
-    db.updateObjects([hltsv_segment])
+    config_db.updateObjects([hltsv_segment])
     
     return hltsv_segment
 
-def add_dcm_segments(db, dcm_segments):
-    hltsv_segment = db.getObject("Segment", "HLT")
+def add_dcm_segments(config_db, dcm_segments):
+    hltsv_segment = config_db.getObject("Segment", "HLT")
     #add all the dcm segments to the hlt segment
     for d_segment in dcm_segments:
         hltsv_segment.Segments.append(d_segment)
        
-    db.updateObjects([hltsv_segment])
+    config_db.updateObjects([hltsv_segment])
--- pm_partition.py
+++ pm_partition.py
@@ -8,47 +8,48 @@ from pm.project import Project
 
 TAGS = [
         'x86_64-slc6-gcc47-opt',
-        'x86_64-slc6-gcc47-dbg',
+        'x86_64-slc6-gcc47-config_dbg',
         'x86_64-slc5-gcc47-opt',
-        'x86_64-slc5-gcc47-dbg',
+        'x86_64-slc5-gcc47-config_dbg',
        ]
 
-def create_counters(db):
+def create_counters(config_db):
     l1_rates = dal.IS_EventsAndRates("L1_counters")
     l1_rates.EventCounter = "DF.HLTSV.Events.LVL1Events"
     l1_rates.Rate = "DF.HLTSV.Events.Rate"
-    db.updateObjects([l1_rates])
+    config_db.updateObjects([l1_rates])
     l2_rates = dal.IS_EventsAndRates("L2_counters")
     l2_rates.EventCounter = "DF.DCM-top_aggregator.DCM.top.sum.ProxL1Events"
     l2_rates.Rate = "DF.DCM_summary_DF_top_sum.L1Rate"
-    db.updateObjects([l2_rates])
+    config_db.updateObjects([l2_rates])
     eb_rates = (dal.IS_EventsAndRates
                 ("DF.DCM-top_aggregator.DCM.top.sum.EB_counters"))
     eb_rates.Rate = "DF.DCM-top_aggregator.DCM.top.sum.EbRate"
     eb_rates.EventCounter = "DF.DCM-top_aggregator.DCM.top.sum.EbEvents"
-    db.updateObjects([eb_rates])
+    config_db.updateObjects([eb_rates])
     ef_rates = (dal.IS_EventsAndRates
                 ("DF.DCM-top_aggregator.DCM.top.sum.EF_counters"))
     ef_rates.Rate = "DF.DCM-top_aggregator.DCM.top.sum.OutRate"
     ef_rates.EventCounter = ""
-    db.updateObjects([ef_rates])
+    config_db.updateObjects([ef_rates])
 
     daq_counters = dal.IS_InformationSources("DAQ_Counters")
     daq_counters.LVL1 = l1_rates
     daq_counters.LVL2 = l2_rates
     daq_counters.EB = eb_rates
     daq_counters.EF = ef_rates
-    db.updateObjects([daq_counters])
+    config_db.updateObjects([daq_counters])
     return daq_counters
 
 def create_partition(**part_args):
-    db = part_args['db']
+    config_db = part_args['config_db']
     partition = dal.Partition(part_args['part_name'])
 
     partition.RepositoryRoot = part_args['repository_root']
 
     partition.Segments = part_args['segments']
-    partition.OnlineInfrastructure = db.getObject("OnlineSegment", "setup")
+    partition.OnlineInfrastructure = config_db.getObject("OnlineSegment",
+                                                         "setup")
     partition.LogRoot = '/logs/${TDAQ_VERSION}'
     partition.WorkingDirectory = '/logs'
     partition.RepositoryRoot = part_args['repository_root']
@@ -58,30 +59,30 @@ def create_partition(**part_args):
 
     df_parameters.MulticastAddress = part_args['multicast_address']
     df_parameters.DefaultDataNetworks = part_args['data_networks']
-    db.updateObjects([df_parameters])
+    config_db.updateObjects([df_parameters])
     partition.DataFlowParameters = df_parameters
 
-    common_params = db.getObject("VariableSet", "CommonParameters")
+    common_params = config_db.getObject("VariableSet", "CommonParameters")
     partition.Parameters.append(common_params)
-    external_params = db.getObject("VariableSet", "External-parameters")
+    external_params = config_db.getObject("VariableSet", "External-parameters")
     partition.Parameters.append(external_params)
 
-    common_env = db.getObject("VariableSet", "CommonEnvironment")
+    common_env = config_db.getObject("VariableSet", "CommonEnvironment")
     partition.ProcessEnvironment.append(common_env)
 
     for tag_name in TAGS:
-        tag = db.getObject("Tag", tag_name)
+        tag = config_db.getObject("Tag", tag_name)
         partition.DefaultTags.append(tag)
 
     partition.DefaultHost = part_args['default_host']
 
-    daq_counters = create_counters(db)
+    daq_counters = create_counters(config_db)
     partition.IS_InformationSource = daq_counters
     
-    pu_db = Project("PuDummy.data.xml")
-    trig_config = pu_db.getObject("TriggerConfiguration", "TrigConf-1")
+    pu_config_db = Project("PuDummy.data.xml")
+    trig_config = pu_config_db.getObject("TriggerConfiguration", "TrigConf-1")
     partition.TriggerConfiguration = trig_config
 
-    partition.Disabled.append(db.getObject("Segment", "ROS"))
-    db.updateObjects([partition])
+    partition.Disabled.append(config_db.getObject("Segment", "ROS"))
+    config_db.updateObjects([partition])
     return partition
