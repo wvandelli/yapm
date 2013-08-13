ff3c9be934660f3e2c75a1c56555ceb408237ecc
Argyris Zardilis <argyris.zardilis@cern.ch>
2013-08-13 18:02:18 +0200

    made it work independently with own includes

--- part_hltsv.py
+++ part_hltsv.py
@@ -1,13 +1,3 @@
-"""
-This module creates a complete partition in a bottom up approach
-according to the farm description it loads from the file provided as
-command-line argument and the other command line arguments. Starts
-from the standalone hlt segments which don't contain any other
-segments and then moves up to the top level segments(HLTSV, ROS) and
-then finally to the partition object.  No error checking yet on the
-sanity of the provided command line arguments and the farm dictionary
-"""
-
 from pm.project import Project
 import argparse
 import pm_ros
@@ -17,13 +7,15 @@ import pm_hlt
 import pm_partition
 import imp
 
-IMPORT_ERROR_MESSAGE = "Couldn't import module provided: %s"
-ATTR_ERROR_MESSAGE = (
-                     """
-                     Couldn't find dictionary with
-                     default name(farm_dict) in module provided"
-                     """
-                     )
+DEFAULT_INCLUDES = ['daq/segments/setup.data.xml',
+                    'daq/schema/hltsv.schema.xml',
+                    'daq/sw/repository.data.xml',
+                    'dcm/schema/dcm_is.schema.xml',
+                    'daq/schema/HLTMPPU.schema.xml',
+                    'daq/schema/MonInfoGatherer.schema.xml',
+                    'daq/schema/dcm.schema.xml',
+                    'daq/sw/tags.data.xml',
+                    'daq/segments/ROS/ROS-LAR-emulated-dc.data.xml']
 
 def get_farm_dict(module_name):
     farm_gen = imp.find_module(module_name)
@@ -34,17 +26,12 @@ def get_farm_dict(module_name):
 
 def create_config_db(args):
     hlt_segments = []
+    farm_dict = get_farm_dict(args.farm_file)
+    print("got farm_dict")
+    
 
-    try:
-        farm_dict = get_farm_dict(args.farm_file)
-    except ImportError:
-        print(IMPORT_ERROR_MESSAGE % args.farm_file)
-    except AttributeError:
-        print(ATTR_ERROR_MESSAGE)
-    finally:
-        return
-
-    full_includes = pm_common.DEFAULT_INCLUDES + args.extra_includes
+    full_includes = DEFAULT_INCLUDES + args.extra_includes
+    
     config_db = Project(args.segment_name + ".data.xml", full_includes)
     if args.local:
         local_host = farm_dict['default_host']
@@ -70,7 +57,6 @@ def create_config_db(args):
                                                farm_dict['hltsv'],
                                                farm_dict['sfos']))
     pm_hltsv.add_dcm_segments(config_db, hlt_segments)
-    part_segments.append(hlt_segment)
 
 
 def get_parser():
