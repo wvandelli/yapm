06d1d398916e8b4aeb2a186066c4e5d87e485d38
Argyris Zardilis <argyris.zardilis@cern.ch>
2013-08-14 18:11:04 +0200

    added a commit after adding new include

--- pm_merge.py
+++ pm_merge.py
@@ -11,11 +11,11 @@ def merge(args):
     partition_name = string.split(args.partition_file, ".")[0]
     part_db = Project(args.partition_file)
     part_db.addInclude(args.segment_file)
+    part_db.commit()
     partition = part_db.getObject("Partition", partition_name)
     hltsv_segment = part_db.getObject("Segment", "HLT")
     partition.Segments.append(hltsv_segment)
     part_db.updateObjects([partition])
-
 
 def get_parser():
     parser = argparse.ArgumentParser()
